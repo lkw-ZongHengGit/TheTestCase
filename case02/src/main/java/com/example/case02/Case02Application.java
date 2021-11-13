65ad5df269fb13bb2b9c5c3209a538ee9aa07f01
lkw-ZongHengGit <mr_liu_kw@163.com>
2021-11-13 20:42:19 +0800

    提交测试

---
+++ case02/src/main/java/com/example/case02/Case02Application.java
@@ -0,0 +1,13 @@
+package com.example.case02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Case02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Case02Application.class, args);
+    }
+
+}
