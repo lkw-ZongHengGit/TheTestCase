7703423bc46a280eba8b11d4e910d1ddc41d93bd
lkw-ZongHengGit <mr_liu_kw@163.com>
2022-01-25 00:19:39 +0800

    新增你好呀方法

--- gmall-user/src/main/java/com/atguigu/gmall/user/aop/SimploController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/aop/SimploController.java
@@ -3,6 +3,6 @@ package com.atguigu.gmall.user.aop;
 public class SimploController {
     public static void main(String[] args) {
         System.out.println("本地分支");
-
+        System.out.println("Nihaoya ~");
     }
 }
